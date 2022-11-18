571db1d35209520254b1ea8248f50c3ae0c81c88
mkowalewski <mateusz.kowalewski@nekken.pl>
2022-11-18 21:53:24 +0100

    Add word to trie tree

--- src/main/java/pl/mkowalewski/sse/trie/Node.java
+++ src/main/java/pl/mkowalewski/sse/trie/Node.java
@@ -4,9 +4,19 @@ class Node {
 
   private static final int ALPHABET_SIZE = 'z' - 'a';
 
-  private int[] edges = new int[ALPHABET_SIZE];
+  private Integer[] edges = new Integer[ALPHABET_SIZE];
 
   public Node() {
   }
 
+  boolean existEdgeFor(char c) {
+    return edges[c - 'a'] != null;
+  }
+
+  public void addEdge(char c, int index) {
+    if (edges[c - 'a'] != null) {
+      throw new RuntimeException(); // TODO: mkowalewski - add custom errors
+    }
+    edges[c - 'a'] = index;
+  }
 }
--- src/main/java/pl/mkowalewski/sse/trie/TrieTree.java
+++ src/main/java/pl/mkowalewski/sse/trie/TrieTree.java
@@ -6,9 +6,25 @@ import java.util.List;
 class TrieTree {
 
   private List<Node> nodes = new ArrayList<Node>();
+  private int indexCount = 0;
 
   public TrieTree() {
     nodes.add(new Node());
+  }
+
+  public void addWord(String s) {
+    // TODO: mkowalewski - check for invalid input?
+    // probably should be in different class
+    s.toLowerCase();
+    for (int i = 0; i < s.length(); i++) {
+      if (nodes.size() > i) {
+        if (!nodes.get(i).existEdgeFor(s.charAt(i))) {
+          nodes.get(i).addEdge(s.charAt(i), indexCount++);
+        }
+      } else {
+        nodes.get(i).addEdge(s.charAt(i), indexCount++);
+      }
+    }
   }
 
 }
