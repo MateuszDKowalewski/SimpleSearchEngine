89530b34b8a3ae9ada2769b9261ad3c63dae090f
mkowalewski <mateusz.kowalewski@nekken.pl>
2022-11-18 21:26:47 +0100

    Add trie tree and nodes class

---
+++ src/main/java/pl/mkowalewski/sse/trie/Node.java
@@ -0,0 +1,12 @@
+package pl.mkowalewski.sse.trie;
+
+class Node {
+
+  private static final int ALPHABET_SIZE = 'z' - 'a';
+
+  private int[] edges = new int[ALPHABET_SIZE];
+
+  public Node() {
+  }
+
+}
---
+++ src/main/java/pl/mkowalewski/sse/trie/TrieTree.java
@@ -0,0 +1,14 @@
+package pl.mkowalewski.sse.trie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TrieTree {
+
+  private List<Node> nodes = new ArrayList<Node>();
+
+  public TrieTree() {
+    nodes.add(new Node());
+  }
+
+}
