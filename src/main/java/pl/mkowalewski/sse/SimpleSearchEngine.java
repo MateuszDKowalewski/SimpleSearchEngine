fcfe77edfdf6acc26398566bce3a3dbbbc4cec4d
mkowalewski <mateusz.kowalewski@nekken.pl>
2022-11-19 16:45:29 +0100

    Delete tests from main

--- src/main/java/pl/mkowalewski/sse/SimpleSearchEngine.java
+++ src/main/java/pl/mkowalewski/sse/SimpleSearchEngine.java
@@ -6,19 +6,10 @@ class SimpleSearchEngine {
 
   public static void main(String[] args) {
     TrieTree t = new TrieTree();
-    System.out.println("Test 1");
     t.addWord("mapa");
-    System.out.println(t);
-    System.out.println("Test 2");
     t.addWord("plan");
-    System.out.println(t);
-    System.out.println("Test 3");
     t.addWord("pole");
-    System.out.println(t);
-    System.out.println("Test 4");
     t.addWord("las");
-    System.out.println(t);
-    System.out.println("Test 5");
     t.addWord("loch");
     System.out.println(t);
   }
