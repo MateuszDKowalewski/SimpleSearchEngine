4f4524fd99948a276d5c537eb34312751072e2ca
mkowalewski <mateusz.kowalewski@nekken.pl>
2022-11-20 16:31:48 +0100

    Configure build and junit5

---
+++ src/test/java/pl/mkowalewski/sse/trie/TrieTreeTest.java
@@ -0,0 +1,18 @@
+package pl.mkowalewski.sse.trie;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TrieTreeTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void test() {
+
+  }
+
+}
