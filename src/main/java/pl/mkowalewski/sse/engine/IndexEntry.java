840d1e0d4c4d68f823fc1f5674d241c6f1d4ab5f
mkowalewski <mateusz.kowalewski@nekken.pl>
2022-11-20 17:07:20 +0100

    Implement IndexEntry.java

--- src/main/java/pl/mkowalewski/sse/engine/IndexEntry.java
+++ src/main/java/pl/mkowalewski/sse/engine/IndexEntry.java
@@ -1,5 +1,16 @@
 package pl.mkowalewski.sse.engine;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
 class IndexEntry {
+
+  private Long documentId;
+  private String content;
+  private float tfIdfScore;
 
 }
