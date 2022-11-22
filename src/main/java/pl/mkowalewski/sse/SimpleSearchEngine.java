4db40068ff975bf0dc68c7350848faba3621879e
mkowalewski <mateusz.kowalewski@nekken.pl>
2022-11-22 15:52:34 +0100

    Create simple gui

--- src/main/java/pl/mkowalewski/sse/SimpleSearchEngine.java
+++ src/main/java/pl/mkowalewski/sse/SimpleSearchEngine.java
@@ -1,24 +1,57 @@
 package pl.mkowalewski.sse;
 
-import java.util.List;
-import pl.mkowalewski.sse.engine.HashMapSearchEngine;
-import pl.mkowalewski.sse.engine.IndexEntry;
-import pl.mkowalewski.sse.engine.SearchEngine;
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import java.io.IOException;
 
 class SimpleSearchEngine {
 
-  public static void main(String[] args) {
-    SearchEngine se = new HashMapSearchEngine();
-    se.indexDocument("Document 1", "the brown fox jumped over the brown dog");
-    se.indexDocument("Document 2", "the lazy brown dog sat in the corner");
-    se.indexDocument("Document 3", "the red fox bit the lazy dog");
-    List<IndexEntry> result = se.search("brown");
-    result.forEach(doc -> {
-      System.out.println(doc.getDocumentId());
-      System.out.println(doc.getContent());
-      System.out.println(doc.getTfIdfScore());
-      System.out.println();
-    });
+  public static void main(String[] args) throws IOException {
+    Terminal terminal = new DefaultTerminalFactory().createTerminal();
+    Screen screen = new TerminalScreen(terminal);
+    TextGraphics tg = screen.newTextGraphics();
+    screen.startScreen();
+
+
+    boolean keepRunning = true;
+    int currentMenuPosition = 0;
+    renderScreen(screen, tg, currentMenuPosition);
+    while (keepRunning) {
+      KeyStroke keyStroke = terminal.pollInput();
+      if (keyStroke != null) {
+        System.out.println(keyStroke);
+        switch (keyStroke.getKeyType()) {
+          case Escape:
+            keepRunning = false;
+            break;
+          case ArrowDown:
+            currentMenuPosition++;
+            renderScreen(screen, tg, currentMenuPosition);
+            System.out.println(currentMenuPosition);
+            break;
+          case ArrowUp:
+            currentMenuPosition--;
+            renderScreen(screen, tg, currentMenuPosition);
+            System.out.println(currentMenuPosition);
+            break;
+        }
+      }
+    }
+    screen.stopScreen();
+  }
+
+  private static void renderScreen(Screen screen, TextGraphics tg, int currentMenuPosition) throws IOException {
+    tg.putString(1, 1, "Simple search engine by Mateusz Kowalewski", SGR.BOLD);
+    tg.putString(1, 5, "Select search engine", currentMenuPosition % 4 == 0 ? SGR.BLINK : SGR.BOLD);
+    tg.putString(1, 6, "Index document from disk", currentMenuPosition % 4 == 1 ? SGR.BLINK : SGR.BOLD);
+    tg.putString(1, 7, "Index document manual", currentMenuPosition % 4 == 2 ? SGR.BLINK : SGR.BOLD);
+    tg.putString(1, 8, "Search phrase in documents", currentMenuPosition % 4 == 3 ? SGR.BLINK : SGR.BOLD);
+    screen.refresh();
   }
 
 }
